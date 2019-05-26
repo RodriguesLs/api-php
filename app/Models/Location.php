dd16cf3e98255e11db208daf033dfcfdf66f9a33
Rodrigues <fr.costa95@gmail.com>
2019-05-26 08:36:17 -0300

    feat(Controller, Model): added model and controller Films
    	Implemented resources to Films, controller and model specifc.
    	In this feature, is possible created a new film, edit then and delete.

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    protected $fillable = [
+        'customer_id',
+        'film_id',
+        'rent_date'
+    ];
+
+    public function rules(){
+
+        return [
+            'customer_id' => 'required',
+            'film_id' => 'required',
+            'rent_date' => 'rent_date'
+        ];
+    }
 }
